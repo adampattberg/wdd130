539258bf8d17737e9143c869ee4c473909717cea
adampattberg <pat23012@byui.edu>
2026-02-21 12:09:11 -0700

    ICE links working

--- personal_project/app.py
+++ personal_project/app.py
@@ -30,6 +30,31 @@ def ice():
 def site_plan_rafting():
     return render_template("site-plan-rafting.html")
 
+# Prophet Card
+@app.route("/prophet-card")
+def prophet_card():
+    return render_template("ice/prophet_cards/nelson.html")
+
+# Apostle Spotlights
+@app.route("/apostle-spotlight")
+def apostle_spotlight():
+    return render_template("ice/apostle_spotlight/apostles.html")
+
+# Favorite Devo
+@app.route("/favorite-devo")
+def favorite_devo():
+    return render_template("ice/favorite_devo/favdevo.html")
+
+# Valentines
+@app.route("/valentines")
+def valentines():
+    return render_template("ice/valentines/index.html")
+
+# Grid Flags
+@app.route("/grid-flags")
+def grid_flags():
+    return render_template("ice/grid_flags/flags.html")
+
 # Contact form submission
 @app.route("/send", methods=["POST"])
 def send():
